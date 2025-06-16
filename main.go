d32311c7ea852d061185ca3c03f20a512af372e6
joydeepVector <joydeep.p@vectoredge.io>
2025-06-17 01:40:13 +0530

    Add gRPC server and cron job for report generation

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/glitchdawg/reportservice/internal/server"
+	pb "github.com/glitchdawg/reportservice/proto"
+	"github.com/robfig/cron/v3"
+	"google.golang.org/grpc"
+)
+
+var predefinedUsers = []string{"user-101", "user-202", "user-303"}
+
+func main() {
+	store := &server.ReportStore{
+		Reports: make(map[string]string),
+	}
+
+	s := grpc.NewServer()
+	reportServer := &server.ReportServiceServer{Store: store}
+	pb.RegisterReportServiceServer(s, reportServer)
+
+	go startCronJob(reportServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	log.Println("gRPC server listening on :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
+
+func startCronJob(s *server.ReportServiceServer) {
+	c := cron.New()
+	c.AddFunc("@every 10s", func() {
+		for _, uid := range predefinedUsers {
+			_, err := s.GenerateReport(nil, &pb.GenerateReportRequest{UserId: uid})
+			if err != nil {
+				log.Printf("Cron failed for user %s: %v", uid, err)
+			}
+		}
+	})
+	c.Start()
+}
